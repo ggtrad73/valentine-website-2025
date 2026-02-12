c18f91d6f952f3ba055e022653064fdcc3f0b137
ggtrad73 <gabytrad23@gmail.com>
2026-02-12 14:39:17 -0600

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://youtu.be/zIEIBKJ13CA?si=RThX8vXbwNHy8dgL", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dgmmp2zyo/video/upload/v1770928626/Yung_Lean_-_Hennessy_Sailor_Moon_Feat._Bladee_Prod._Acea_zvpw2c.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
